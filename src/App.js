26327447ed5d60306a5c2d6b6869d48a617498a7
Lashon Hudson <lashonh74@gmail.com>
2021-03-16 12:09:16 -0400

    Created a webpage that displays questions and allows conditional rendering to show answers

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+function App() {
+  const [questions, setQuestions] = useState(data)
+  const {id, title, info } = questions
+
+  return <main>
+    
+     <div className="container">
+      <h3>Questions and answers about login</h3>
+      <section className="info">
+        {
+          questions.map((question) => {
+            return <SingleQuestion key={ id} {...question} />
+          })
+        }
+      </section>
+     </div>
+    </main>;
+}
+
+export default App;
